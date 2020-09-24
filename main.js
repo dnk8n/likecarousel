9687a93757688fdc41cc8afa7bac919bbc4270d9
Dean Kayton <deankayton@gmail.com>
2020-09-24 20:08:10 +0200

    Convert to ES6 way of doing things

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import "https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js";
+import {Carousel} from "./carousel.js";
+
 let board = document.querySelector("#board");
 
 let carousel = new Carousel(board);

b260eb2cf8dd2b65620c43f40059d0698ba0d9c6
Dean Kayton <deankayton@gmail.com>
2020-09-24 20:13:00 +0200

    Formatting and specific device specs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import "https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js";
-import {Carousel} from "./carousel.js";
+import { Carousel } from "./carousel.js";
 
 let board = document.querySelector("#board");
 

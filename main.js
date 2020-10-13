26a5c588442cdde83f76bc17b805e6e15563e87f
Dean Kayton <deankayton@gmail.com>
2020-10-13 13:14:07 +0200

    Go through an image proxy, which uses its cloud speed internet to download the original image (there are often 2 or 3 redirects involved with picsim which can take some time on a bad client connection), then serves it in optimized webp format at the optimal resolution

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 import { Carousel } from "./carousel.js";
 
-function* cardGenerator() {
+function* cardGenerator(cardClass) {
   while (true) {
     const card = document.createElement("div");
-    card.classList.add("card");
-    card.style.backgroundImage =
-      "url('https://picsum.photos/320/320/?random=" +
-      Math.round(Math.random() * 1000000) +
-      "')";
+    const width = window.devicePixelRatio * (board.clientWidth - 100);
+    const height = window.devicePixelRatio * (board.clientHeight - 100);
+    const random = Math.round(Math.random() * 1000000);
+    const srcUrl = `https://picsum.photos/${width}/${height}?random=${random}`;
+    const proxyUrl = `https://img.dnk8n.dev/api?width=${width}&height=${height}&url=${srcUrl}`;
+
+    card.classList.add(cardClass);
+    card.style.backgroundImage = `url(${proxyUrl})`;
+
     yield card;
   }
 }
 
 const board = document.querySelector("#board");
 const cardClass = "card";
-const cardGeneratorObj = cardGenerator();
+const cardGeneratorObj = cardGenerator(cardClass);
 
 new Carousel(board, cardClass, cardGeneratorObj);

ec0f662c1db7a078be35ba260b1deb05f82e7acd
Dean Kayton <deankayton@gmail.com>
2020-10-14 15:33:39 +0200

    Make sure we are working with ints, not floats!

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import { Carousel } from "./carousel.js";
 function* cardGenerator(cardClass) {
   while (true) {
     const card = document.createElement("img");
-    const width = window.devicePixelRatio * (board.clientWidth - 100);
-    const height = window.devicePixelRatio * (board.clientHeight - 100);
+    const width = Math.floor(window.devicePixelRatio * (board.clientWidth - 100));
+    const height = Math.floor(window.devicePixelRatio * (board.clientHeight - 100));
     const random = Math.round(Math.random() * 1000000);
     const srcUrl = `https://picsum.photos/${width}/${height}?random=${random}`;
     const proxyUrl = `https://img.dnk8n.dev/api?width=${width}&height=${height}&url=${srcUrl}`;

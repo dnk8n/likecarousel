498ff7208ea289f303a6c4e0c6ec448b934fb280
Dean Kayton <deankayton@gmail.com>
2020-10-14 15:20:56 +0200

    For some reason the previous code was not working on my mobile. I suspect this will fix it, but don't understand why...

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { Carousel } from "./carousel.js";
 
 function* cardGenerator(cardClass) {
   while (true) {
-    const card = document.createElement("div");
+    const card = document.createElement("img");
     const width = window.devicePixelRatio * (board.clientWidth - 100);
     const height = window.devicePixelRatio * (board.clientHeight - 100);
     const random = Math.round(Math.random() * 1000000);
@@ -10,7 +10,7 @@ function* cardGenerator(cardClass) {
     const proxyUrl = `https://img.dnk8n.dev/api?width=${width}&height=${height}&url=${srcUrl}`;
 
     card.classList.add(cardClass);
-    card.style.backgroundImage = `url(${proxyUrl})`;
+    card.src = proxyUrl
 
     yield card;
   }
